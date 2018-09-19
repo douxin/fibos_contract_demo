28fa0bc2d5f39556ec8db11c6593f5f26ee3b44d
Xin Dou <master2dou@gmail.com>
2018-09-19 22:54:47 +0800

    add comment for start script

--- start.js
+++ start.js
@@ -2,8 +2,8 @@ const fibos = require('fibos');
 
 fibos.load('http');
 fibos.load('chain', {
-    'delete-all-blocks': true,
-    'contracts-console': true,
+    // 'delete-all-blocks': true,
+    'contracts-console': true, // 开启此项，可以在 console 中查看 contract 运行情况
 });
 fibos.load('net');
 fibos.load('chain_api');
